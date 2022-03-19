af276065065512cd1e464cdd2dd54bc32e33bbc2
vladalexgor@ya.ru <vladalexgor@ya.ru>
2022-03-19 16:51:31 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection/Main.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var pickedPoint = uidoc.Selection.PickPoint(ObjectSnapTypes.Endpoints, "Выберите точку");
+            TaskDialog.Show("Point info", $"X: {pickedPoint.X}, Y: {pickedPoint.Y}, Z: {pickedPoint.Z}");
+
+            //IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, new WallFilter(), "Выберите стены");
+            //var wallList = new List<Wall>();
+
+            //string info = string.Empty;
+            //foreach (var selectedElement in selectedElementRefList)
+            //{
+            //    Wall oWall = doc.GetElement(selectedElement) as Wall;
+            //    wallList.Add(oWall);
+            //    var width = UnitUtils.ConvertFromInternalUnits(oWall.Width, UnitTypeId.Millimeters);
+            //    info += $"Name: {oWall.Name}, width: {width}{Environment.NewLine}";
+            //}
+
+            //info += $"Количество: {wallList.Count}";
+
+            //TaskDialog.Show("Selection", info);
+
+
+            return Result.Succeeded;
+        }
+    }
+}
